69883a597f32faf161321ba843c7b7305867b9c8
leinadx4 <daniel.jauregui@outlook.com>
2015-04-15 16:56:45 -0400

    Update requests

--- src/game.py
+++ src/game.py
@@ -14,12 +14,13 @@ class Game(object):
 
     def generate_game(self):
         """Call to make_puzzle with a empty board to generate game
-        with level defined and it will return a array e.g.: Array: [4, 9, 3, None, 5, ...., 8, 8, None, None, 1]
+        with level defined and it will return a array e.g.:
+        Array: [4, 9, 3, None, 5, ...., 8, 8, None, None, 1]
         Where the position are from 0 to 80
 
         return -- Array e.g.: [4, 9, 3, None, 5, ...., 8, 8, None, None, 1]
         """
-        return self.make_puzzle(self.solution([None] * 81))
+        return self.make_puzzle(self.get_solution([None] * 81))
 
     def make_puzzle(self, board):
         """Get the solved board and remove the range of values
@@ -35,6 +36,7 @@ class Game(object):
         # Deduced will use basic board for iteration conditional and fill it for each value found
         deduced = [None] * 81
         order = random.sample(xrange(81), 81)
+        print order
         for position in order:
             if deduced[position] is None:
                 puzzle.append((position, board[position]))
@@ -63,7 +65,7 @@ class Game(object):
             board[position] = number
         return board
 
-    def solution(self, board):
+    def get_solution(self, board):
         """Get an empty array with 81 positions and call
         solve_board method to get a solved sudoku.
 
@@ -93,7 +95,7 @@ class Game(object):
         it will create all game until a board does not contains None values
 
         Keyword arguments:
-        remembered -- Array e.g.: [[None, None,..., None, None]]
+        remembered -- Contain all boards guesses, it is an Array e.g.: [[None, None,..., None, None]]
         return  -- Array e.g.: [4, 9, 3, 4, 5, ...., 8, 7, 4, 8, 1]
         """
         while len(remembered) > 0:
@@ -122,40 +124,152 @@ class Game(object):
             stuck, guess, count = True, None, 0
             # fill in any spots determined by direct conflicts
             allowed, needed = self.calculate_bits(board)
-            for position in xrange(81):
-                if None == board[position]:
-                    numbers = self.list_bits(allowed[position])
-                    if len(numbers) == 0:
-                        return []
-                    elif len(numbers) == 1:
-                        board[position] = numbers[0]
-                        stuck = False
-                    elif stuck:
-                        guess, count = self.pick_better(guess, count, [(position, number) for number in numbers])
+            board, allowed, stuck, guess, count = self.move_on_board(board, allowed, stuck, guess, count)
             if not stuck:
                 allowed, needed = self.calculate_bits(board)
             # fill in any spots determined by elimination of other locations
-            for axis in xrange(3):
-                for x in xrange(9):
-                    numbers = self.list_bits(needed[axis * 9 + x])
-                    for number in numbers:
-                        bit = 1 << number
-                        spots = []
-                        for y in xrange(9):
-                            position = self.position_for(x, y, axis)
-                            if allowed[position] & bit:
-                                spots.append(position)
-                        if len(spots) == 0:
-                            return []
-                        elif len(spots) == 1:
-                            board[spots[0]] = number
-                            stuck = False
-                        elif stuck:
-                            guess, count = self.pick_better(guess, count, [(position, number) for position in spots])
+            board, stuck, guess, count = self.move_on_board_for_axis(needed ,allowed ,board,guess, count, stuck)
             if stuck:
-                if guess is not None:
-                    random.shuffle(guess)
-                return guess
+                return self.get_is_stuck(guess)
+
+    def get_is_stuck(self, guess):
+        """Ask about guess value
+
+        Keyword arguments:
+        guess -- inherited from deduce function
+        return  -- return an integer from 1 to 9
+        """
+        if guess is not None:
+            random.shuffle(guess)
+        return guess
+
+    def move_on_board_for_axis(self,needed ,allowed ,board,guess, count, stuck):
+        """Move on board for each axis and guess needed and allowed values
+
+        Keyword arguments:
+        needed -- Array of numbers needed to resolve puzzle
+        allowed -- Array of numbers allowed to resolve puzzle
+        board -- inherited from deduce function
+        guess -- Array of random numbers from 1 to 9
+        count -- Count how many times pick the best option of guesses
+        stuck -- Verify if game stuck returning true or false
+        return  -- Tuple of (board, stuck, guess, count)
+        """
+        for axis in xrange(3):
+            board, stuck, guess, count = self.move_on_x_position(needed, allowed, board, guess, count, axis, stuck)
+        return board, stuck, guess, count
+
+    def move_on_x_position(self,needed ,allowed ,board, guess, count, axis, stuck):
+        """Move on axis position for each axis x  and guess needed and allowed values
+
+        Keyword arguments:
+        needed -- inherited from move_on_board_for_axis function
+        allowed -- inherited from move_on_board_for_axis function
+        board -- inherited from move_on_board_for_axis function
+        guess -- inherited from move_on_board_for_axis function
+        count -- inherited from move_on_board_for_axis function
+        axis -- inherited from move_on_board_for_axis function
+        stuck -- inherited from move_on_board_for_axis function
+        return  -- Tuple of (board, stuck, guess, count)
+        """
+        for x in xrange(9):
+            numbers = self.list_bits(needed[axis * 9 + x])
+            zero,board,stuck,guess,count =  self.move_for_each_numbers(allowed ,board, guess, count, axis, x, numbers, stuck)
+            if zero == 0:
+                return []
+        return board,stuck,guess,count
+
+    def move_for_each_numbers(self, allowed ,board, guess, count, axis,x, numbers, stuck):
+        """Move on axis position for each axis x  and guess needed and allowed values
+
+        Keyword arguments:
+        needed -- inherited from move_on_x_position function
+        allowed -- inherited from move_on_x_position function
+        board -- inherited from move_on_x_position function
+        guess -- inherited from move_on_x_position function
+        count -- inherited from move_on_x_position function
+        x  -- inherited from move_on_x_position function
+        axis -- inherited from move_on_x_position function
+        stuck -- inherited from move_on_x_position function
+        return  -- Tuple of (spots_empty,board,stuck,guess,count) where spots_empty is a flag
+        """
+        spots_empty = 1
+        for number in numbers:
+            bit = 1 << number
+            spots = []
+            spots = self.move_for_y_position(allowed, x, axis, bit, spots)
+            if len(spots) == 0:
+                spots_empty = 0
+            elif len(spots) == 1:
+                board[spots[0]] = number
+                stuck = False
+            elif stuck:
+                guess, count = self.pick_better(guess, count, [(position, number) for position in spots])
+        return spots_empty,board,stuck,guess,count
+
+    def move_for_y_position(self,allowed, x, axis, bit, spots):
+        """Move on axis position for each axis y  and guess allowed values
+
+        Keyword arguments:
+        allowed -- inherited from move_for_each_numbers function
+        x  -- inherited from move_for_each_numbers function
+        axis -- inherited from move_for_each_numbers function
+        bit -- get a 0 or 1 for flag deduction
+        spots -- It has a candidate numbers to be chosen
+        return  -- a list os spots for allowed values
+        """
+        for y in xrange(9):
+            position = self.set_position_for(x, y, axis)
+            if allowed[position] & bit:
+                spots.append(position)
+        return spots
+
+    def move_on_board(self, board, allowed, stuck, guess, count):
+        """Move on board for each position from 1 to 81
+
+        Keyword arguments:
+        allowed -- Array of numbers allowed to resolve puzzle
+        board -- inherited from deduce function
+        guess -- Array of random numbers from 1 to 9
+        count -- Count how many times pick the best option of guesses
+        stuck -- Verify if game stuck returning true or false
+        return  -- Tuple of (board, allowed, stuck, guess, count)
+        """
+        for position in xrange(81):
+            result = self.verify_board(position, board, allowed, stuck, guess, count)
+            if result[0] == 0:
+                return []
+            if result[0] == 1:
+                board = result[1]
+                stuck = result[2]
+            if result[0] == 2:
+                guess = result[1]
+                count = result[2]
+        return board, allowed, stuck, guess, count
+
+    def verify_board(self, position, board, allowed, stuck, guess, count):
+        """Move on board for each position from 1 to 81
+
+        Keyword arguments:
+        position -- inherited from move_on_board function
+        allowed -- inherited from move_on_board function
+        board -- inherited from move_on_board function
+        guess -- inherited from move_on_board function
+        count -- inherited from move_on_board function
+        stuck -- inherited from move_on_board function
+        return  -- Tuple of (zero, board, stuck, guess, count) where zero is a flag
+        """
+        zero = 1
+        if None == board[position]:
+            numbers = self.list_bits(allowed[position])
+            if len(numbers) == 0:
+                zero = 0
+            elif len(numbers) == 1:
+                board[position] = numbers[0]
+                stuck = False
+            elif stuck:
+                guess, count = self.pick_better(guess, count, [(position, number) for number in numbers])
+        return zero, board, stuck, guess, count
 
     def calculate_bits(self, board):
         """Get a board that require verify if a position is allowed according with
@@ -171,7 +285,7 @@ class Game(object):
                 bits = self.axis_missing(board, x, axis)
                 needed.append(bits)
                 for y in xrange(9):
-                    allowed[self.position_for(x, y, axis)] &= bits
+                    allowed[self.set_position_for(x, y, axis)] &= bits
         return allowed, needed
 
     def axis_missing(self, board, x, axis):
@@ -185,12 +299,12 @@ class Game(object):
         """
         bits = 0
         for y in xrange(9):
-            current_value = board[self.position_for(x, y, axis)]
+            current_value = board[self.set_position_for(x, y, axis)]
             if current_value is not None:
                 bits |= 1 << current_value
         return 511 ^ bits
 
-    def position_for(self, x, y, axis=0):
+    def set_position_for(self, x, y, axis=0):
         """Determine of position in sudoku for current arguments.
 
         Keyword arguments:
