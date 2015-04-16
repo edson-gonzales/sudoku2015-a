42d131be8f4803a98f7e4ee32f92567475bc32a1
daniel-jauregui <daniel.jauregui@outlook.com>
2015-04-15 22:31:48 -0400

    no message

--- src/board.py
+++ src/board.py
@@ -8,34 +8,57 @@ from utils.singleton import Singleton
 
 class Board(object):
 
-    #__metaclass__ = Singleton
-
-    def __init__(self, board=None):
+    def __init__(self, board, code='*'):
         self.board = board
+        self.code = code
 
-    def print_code(self, number):
-        if number is 0:
-            return '*'
-        return str(number)
+    def print_ui(self):
+        self.print_title()
+        self.print_top_menu()
+        self.print_board()
 
-    def print_board(self):
-        out = '-----------------------\n'
-        y = 8
-        for matrix in xrange(81):
-            out += self.print_code(self.board[matrix]) + ' '
-            if (matrix % 3) == 2:
-                    out += "| "
-            if matrix == y:
-                    out += '\n'
-                    if ((matrix >= 19) and (matrix <= 27)) or ((matrix >= 45) and (matrix <= 54)):
-                            out += '------+-------+--------\n'
-                    y += 9
-        out += '-----------------------\n'
-        return out
+    def print_title(self):
+        spaces = '      '
+        title = '\n' + spaces
+        title += chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(187)+chr(219)+chr(219)+chr(187)
+        title += chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(187)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)
+        title += chr(219)+chr(187)+chr(32)+chr(32)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(187)
+        title += chr(32)+chr(219)+chr(219)+chr(187)+chr(32)+chr(32)+chr(219)+chr(219)+chr(187)+chr(219)+chr(219)
+        title += chr(187)+chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(187)+'\n'
+        title += spaces
+        title += chr(219)+chr(219)+chr(201)+chr(205)+chr(205)+chr(205)+chr(205)+chr(188)+chr(219)+chr(219)+chr(186)
+        title += chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(186)+chr(219)+chr(219)+chr(201)+chr(205)+chr(205)
+        title += chr(219)+chr(219)+chr(187)+chr(219)+chr(219)+chr(201)+chr(205)+chr(205)+chr(205)+chr(219)+chr(219)
+        title += chr(187)+chr(219)+chr(219)+chr(186)+chr(32)+chr(219)+chr(219)+chr(201)+chr(188)+chr(219)+chr(219)
+        title += chr(186)+chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(186)+'\n'
+        title += spaces
+        title += chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(187)+chr(219)+chr(219)+chr(186)
+        title += chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(186)+chr(219)+chr(219)+chr(186)+chr(32)+chr(32)
+        title += chr(219)+chr(219)+chr(186)+chr(219)+chr(219)+chr(186)+chr(32)+chr(32)+chr(32)+chr(219)+chr(219)
+        title += chr(186)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(201)+chr(188)+chr(32)+chr(219)+chr(219)
+        title += chr(186)+chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(186)+'\n'
+        title += spaces
+        title += chr(200)+chr(205)+chr(205)+chr(205)+chr(205)+chr(219)+chr(219)+chr(186)+chr(219)+chr(219)+chr(186)
+        title += chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(186)+chr(219)+chr(219)+chr(186)+chr(32)+chr(32)+chr(219)
+        title += chr(219)+chr(186)+chr(219)+chr(219)+chr(186)+chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(186)
+        title += chr(219)+chr(219)+chr(201)+chr(205)+chr(219)+chr(219)+chr(187)+chr(32)+chr(219)+chr(219)+chr(186)
+        title += chr(32)+chr(32)+chr(32)+chr(219)+chr(219)+chr(186)+'\n'
+        title += spaces
+        title += chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(186)+chr(200)+chr(219)+chr(219)
+        title += chr(219)+chr(219)+chr(219)+chr(219)+chr(201)+chr(188)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)
+        title += chr(219)+chr(201)+chr(188)+chr(200)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(201)
+        title += chr(188)+chr(219)+chr(219)+chr(186)+chr(32)+chr(32)+chr(219)+chr(219)+chr(187)+chr(200)+chr(219)
+        title += chr(219)+chr(219)+chr(219)+chr(219)+chr(219)+chr(201)+chr(188)+'\n'
+        title += spaces
+        title += chr(200)+chr(205)+chr(205)+chr(205)+chr(205)+chr(205)+chr(205)+chr(188)+chr(32)+chr(200)+chr(205)
+        title += chr(205)+chr(205)+chr(205)+chr(205)+chr(188)+chr(32)+chr(200)+chr(205)+chr(205)+chr(205)+chr(205)
+        title += chr(205)+chr(188)+chr(32)+chr(32)+chr(200)+chr(205)+chr(205)+chr(205)+chr(205)+chr(205)+chr(188)
+        title += chr(32)+chr(200)+chr(205)+chr(188)+chr(32)+chr(32)+chr(200)+chr(205)+chr(188)+chr(32)+chr(200)
+        title += chr(205)+chr(205)+chr(205)+chr(205)+chr(205)+chr(188)+chr(32)+'\n'
+        print title
 
     def print_top_menu(self):
-
-        left_spaces = '          '
+        left_spaces = '           '
         # Top Menu
         top_menu = ''
         top_menu += left_spaces + chr(201)
@@ -51,7 +74,8 @@ class Board(object):
         for line_vertical in xrange(8):
             top_menu += chr(205)
         top_menu += chr(187) + '\n'
-        top_menu += left_spaces + chr(186)+' N:NewGame '+chr(186)+' H:Hints '+chr(186)+' B:Back '+chr(186)+' E:Exit '+chr(186) + '\n'
+        top_menu += left_spaces + chr(186) + ' N:NewGame ' + chr(186) + ' H:Hints '
+        top_menu += chr(186)+' B:Back ' + chr(186) + ' E:Exit ' + chr(186) + '\n'
         top_menu += left_spaces + chr(200)
         for line_vertical in xrange(11):
             top_menu += chr(205)
@@ -67,30 +91,72 @@ class Board(object):
         top_menu += chr(188) + '\n'
         print top_menu
 
-        characters = ('A','B','C','D','E','F','G','H')
+    def print_board(self):
+        for x in xrange(81):
+            if self.board[x] == 0:
+                self.board[x] = self.code
+        left_spaces = '          '
+        characters = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
         board_ui = left_spaces + '   ' + '  1 '+'  2 '+'  3 '+'  4 '+'  5 '+'  6 '+'  7 '+'  8 '+'  9 '+'\n'
-        board_ui += left_spaces +'   ' + chr(201)
+        board_ui += left_spaces + '   ' + chr(201)
         for n in xrange(8):
             for l in xrange(3):
                 board_ui += chr(205)
-            board_ui += chr(203)
+            if n == 2 or n == 5:
+                board_ui += chr(203)
+            else:
+                board_ui += chr(205)
         board_ui += chr(205) + chr(205) + chr(205)
         board_ui += chr(187) + '\n'
+        position = 0
         for y in xrange(8):
-            board_ui += left_spaces +' '+characters[y]+' ' + chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186) + '\n'
-            board_ui += left_spaces +'   ' + chr(204)
-            for n in xrange(8):
-                for l in xrange(3):
-                    board_ui += chr(205)
-                board_ui += chr(206)
-            board_ui += chr(205) + chr(205) + chr(205)
-            board_ui += chr(185) + '\n'
-        board_ui += left_spaces +'   ' + chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186)+'   '+chr(186) + '\n'
-        board_ui += left_spaces +'   ' + chr(200)
+            board_ui += left_spaces + ' ' + characters[y]+' ' + chr(186)+' ' + str(self.board[position])
+            board_ui += ' '+chr(179)
+            board_ui += ' ' + str(self.board[position+1]) + ' '+chr(179)+' ' + str(self.board[position+2])
+            board_ui += ' ' + chr(186)
+            board_ui += ' ' + str(self.board[position+3]) + ' '+chr(179)+' ' + str(self.board[position+4])
+            board_ui += ' ' + chr(179)
+            board_ui += ' ' + str(self.board[position+5]) + ' '+chr(186)+' ' + str(self.board[position+6])
+            board_ui += ' ' + chr(179)
+            board_ui += ' ' + str(self.board[position+7]) + ' '+chr(179)+' ' + str(self.board[position+8])
+            board_ui += ' ' + chr(186) + '\n'
+            board_ui += left_spaces + '   ' + chr(204)
+            position += 9
+            if y == 2 or y == 5:
+                for n in xrange(8):
+                    for l in xrange(3):
+                        board_ui += chr(205)
+                    if n == 2 or n == 5:
+                        board_ui += chr(206)
+                    else:
+                        board_ui += chr(205)
+                board_ui += chr(205) + chr(205) + chr(205)
+                board_ui += chr(185) + '\n'
+            else:
+                for n in xrange(8):
+                    for l in xrange(3):
+                        board_ui += chr(196)
+                    if n == 2 or n == 5:
+                        board_ui += chr(206)
+                    else:
+                        board_ui += chr(197)
+                board_ui += chr(196) + chr(196) + chr(196)
+                board_ui += chr(185) + '\n'
+
+        board_ui += left_spaces + ' I ' + chr(186) + ' ' + str(self.board[position]) + ' ' + chr(179) + ' '
+        board_ui += str(self.board[position+1]) + ' ' + chr(179) + ' ' + str(self.board[position+2]) + ' ' + chr(186)
+        board_ui += ' ' + str(self.board[position+3]) + ' ' + chr(179) + ' ' + str(self.board[position+4]) + ' '
+        board_ui += chr(179) + ' ' + str(self.board[position+5]) + ' ' + chr(186) + ' ' + str(self.board[position+6])
+        board_ui += ' '+chr(179)+' ' + str(self.board[position+7]) + ' ' + chr(179) + ' ' + str(self.board[position+8])
+        board_ui += ' ' + chr(186) + '\n'
+        board_ui += left_spaces + '   ' + chr(200)
         for n in xrange(8):
             for l in xrange(3):
                 board_ui += chr(205)
-            board_ui += chr(202)
+            if n == 2 or n == 5:
+                board_ui += chr(202)
+            else:
+                board_ui += chr(205)
         board_ui += chr(205) + chr(205) + chr(205)
         board_ui += chr(188) + '\n'
         print board_ui
