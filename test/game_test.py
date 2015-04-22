a4f3929ce4337c0625370f53bb8c550fc2ed0793
leinadx4 <daniel.jauregui@outlook.com>
2015-04-22 18:50:36 -0400

    update TCs

--- test/game_test.py
+++ test/game_test.py
@@ -11,3 +11,12 @@ class GameTest(unittest.TestCase):
     def test_generate_game_create_and_array_of_81_items(self):
         game = Game()
         self.assertEqual(81, len(game.generate_game()))
+
+"""
+
+Verify from boar for each number of column is not duplicated in the row
+Verify from boar for each number of row is not duplicated in the column
+Verify from boar for each number in the block has not duplicate
+
+
+"""
