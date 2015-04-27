6c2cafa5b9a3d1361c95ebad891a55bbd7e9c1e1
unknown <Pamela Quintana@PQUINTANAD-DH01.jalasoft.local>
2015-04-27 10:24:35 -0400

    Backtracking algorithm and test updated

--- src/solver/algorithms/Backtracking_solver.py
+++ src/solver/algorithms/Backtracking_solver.py
@@ -1,9 +1,3 @@
-"""
-Usage:
-A properly formatted string is 9 lines of 9 numbers or dashes separated by blanks,
-where a dash indicates a blank location in the puzzle.
-"""
-
 import sys
 
 
@@ -14,56 +8,35 @@ class BacktrackingSolver(object):
         self.blanks = []  # Initialize blank spots
         self.initPuzzleArray()  # Construct an array of 0 0 0 0 0...
 
-        # Initialize the metrics
-        self.pathLengths = []  # Hold all the path lengths for metrics
-        self.currentPathLength = 0  # Hold the local path length
-        self.blanks = self.getEmptyCells(self.puzzle)  # Construct the list of blanks
-
-    #######################################################
-    # THE BACKTRACKING SOLUTION                           #
-    #######################################################
-    """
-    Solve Sudoku using plain backtracking
+    def solve(self, index=0):
+        """
+    Solve Sudoku using plain backtracking.
     Uses a recursive algorithm to try each value 1-9 in each blank square,
     backtracking when it was not able to put in any value into the square.
-    """
-
-    def solve(self, index=0):
+        """
         # Found a solution if we have gone past the last blank
-        # print(index)  # index = 0
         if index > len(self.blanks) - 1:
-            print "entered to here " + str(index) + str(len(self.blanks))
-            self.endAlgorithm()
+            result = self.endAlgorithm()
+            return result
             # return self.outputSolution()
 
         # Haven't found a solution yet; get coords of the blank
-        print "myValue printed" + str(index)
         row = self.blanks[index][0]
         col = self.blanks[index][1]
-        int(col)
-
         # Try numbers 1-9.
         for num in range(1, 10):
-            if self.puzzleValid(row, col, num):
-                # If the number is valid, increment current path by 1.
-                self.currentPathLength += 1
-                self.puzzle[row][col] = num
-                self.solve(index + 1)
+            if self.puzzle[row][col] == 0:
+                if self.puzzleValid(row, col, num):
+                    self.puzzle[row][col] = num
+                    self.solve(index + 1)
 
-        # No number found...set back to 0 and return to the previous blank
-        self.pathLengths.append(self.currentPathLength)  # Add the current path length to the overall list.
-        self.currentPathLength -= 1  # -1 from path.
         index -= 1
         self.puzzle[row][col] = 0
 
-    #######################################################
-    # CSP VALIDATORS                                      #
-    #######################################################
-    """
-    Check if the current puzzle is legal after placing num in (row, col).
-    """
-
     def puzzleValid(self, row, col, num):
+        """
+    CSP VALIDATORS - Check if the current puzzle is legal after placing num in (row, col).
+        """
         valid = False
         if num == 0:
             return True
@@ -76,33 +49,30 @@ class BacktrackingSolver(object):
 
         return valid
 
-    """
-    Check if num is a legal value for the given row.
-    """
-
     def checkRow(self, row, num):
+        """
+    CSP VALIDATORS - Check if num is a legal value for the given row.
+        """
         for col in range(9):
             currentValue = self.puzzle[row][col]
             if num == currentValue:
                 return False
         return True
 
-    """
-    Check if num is a legal value for the given column.
-    """
-
     def checkColumn(self, col, num):
+        """
+    CSP VALIDATORS - Check if num is a legal value for the given column.
+        """
         for row in range(9):
             currentValue = self.puzzle[row][col]
             if num == currentValue:
                 return False
         return True
 
-    """
-    Check if num is a legal value for the box (one of 9 boxes) containing given row/col
-    """
-
     def checkBox(self, row, col, num):
+        """
+    CSP VALIDATORS - Check if num is a legal value for the box (one of 9 boxes) containing given row/col
+        """
         row = (row / 3) * 3
         col = (col / 3) * 3
 
@@ -112,24 +82,17 @@ class BacktrackingSolver(object):
                     return False
         return True
 
-    #######################################################
-    # ALGORITHM HELPERS                                   #
-    #######################################################
-    """
-    Generic method to end the algorithm in process,
-    calculate the running time, output the solution file,
-    print the metrics and exit.
-    """
-
     def endAlgorithm(self):
-        self.pathLengths.append(self.currentPathLength)  # Append the final path's length
+        """
+    ALGORITHM HELPERS - Generic method to end the algorithm in process and output the solution
+        """
         self.outputSolution()
-        sys.exit(0)
+        return True
 
-    """
-    Get all the empty cells in the puzzle.
-    """
     def getEmptyCells(self, puzzle):
+        """
+    ALGORITHM HELPERS - Get all the empty cells in the puzzle.
+        """
         emptyCells = []
         for i in range(9):
             for j in range(9):
@@ -137,14 +100,10 @@ class BacktrackingSolver(object):
                     emptyCells.append((i, j))
         return emptyCells
 
-    #######################################################
-    # I/O HELPERS                                         #
-    #######################################################
-    """
-    Constructs the blank puzzle array to have 0 0 0 0 0...for all rows and columns
-    """
-
     def initPuzzleArray(self):
+        """
+    I/O HELPERS - Constructs the blank puzzle array to have 0 0 0 0 0...for all rows and columns
+        """
         del self.puzzle[:]
         for i in range(9):
             self.puzzle.append([])
@@ -152,17 +111,15 @@ class BacktrackingSolver(object):
                 self.puzzle[i].append([])
                 self.puzzle[i][j] = 0
 
-    """
-    Loads a puzzle file into the program and makes sure that
+    def loadPuzzle(self, inputstr):
+        """
+    I/O HELPERS - Loads a puzzle file into the program and makes sure that
     it is valid.
-    """
-
-    def loadPuzzle(self, inputstr):  # puzzleFile
+        """
         possibleTokens = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
         puzzleText = inputstr.split("\n")
         for row in range(9):
             lineTokens = puzzleText[row + 1].split()  # take care because row+1, before was row
-            # print lineTokens
             if len(lineTokens) != 9:
                 print "Improper file format! Line length must always be 9"
                 sys.exit(1)
@@ -177,16 +134,16 @@ class BacktrackingSolver(object):
                     sys.exit(1)
 
                 self.puzzle[row][col] = int(token)
+        self.blanks = self.getEmptyCells(self.puzzle)  # Construct the list of blanks
         print ""
 
-    """
-    Write the solution to a string
-    """
-
     def outputSolution(self):
+        """
+    I/O HELPERS - Write the solution to a string
+        """
+        rowString = ""
         for i in range(9):
-            rowString = ""
             for j in range(9):
                 rowString += str(self.puzzle[i][j]) + " "
-                # print rowString
+            rowString += "\n"
         return rowString
--- test/Backtracking_solver_test.py
+++ test/Backtracking_solver_test.py
@@ -8,17 +8,18 @@ from Backtracking_solver import BacktrackingSolver
 class BackTrackingSolverTest(unittest.TestCase):
  
     game1 = """
-- - 6 2 - 1 - - -
-8 - - - - - - 7 1
-- - 1 7 - - - 3 2
-- - 7 - 3 - - 4 -
-- 5 - - - - - 8 -
-- 8 - - 4 - 7 - -
-4 6 - - - 5 8 - -
-1 7 - - - - - - 4
-- - - 4 - 6 5 - -"""
+- - 6 2 9 1 4 5 8
+8 4 2 - 5 3 9 7 1
+5 9 1 7 8 4 6 3 2
+9 1 7 5 - 8 2 4 6
+2 5 4 9 6 7 1 8 3
+6 8 3 1 4 2 7 9 5
+4 6 9 3 - 5 8 2 7
+1 7 5 8 2 9 - 6 4
+3 2 8 4 7 6 - 1 9"""
 
-    game1_solution = """7 3 6 2 9 1 4 5 8
+    game1_solution = """
+7 3 6 2 9 1 4 5 8
 8 4 2 6 5 3 9 7 1
 5 9 1 7 8 4 6 3 2
 9 1 7 5 3 8 2 4 6
@@ -41,20 +42,16 @@ class BackTrackingSolverTest(unittest.TestCase):
 5 0 7   0 0 0   0 0 0"""
 
     def test_backtracking_algorithm_can_solve_sudoku_game1(self):
-        print "my testing"
         Backtracking_Solver = BacktrackingSolver()
-        print "my testing"
         Backtracking_Solver.loadPuzzle(self.game1)
-        print "my testing"
-        # Backtracking_Solver.solve()
-        print "my testing"
-        self.assertEquals(self.game1_solution, Backtracking_Solver.solve())
+        Backtracking_Solver.solve()
+        self.assertEquals(self.game1_solution, Backtracking_Solver.outputSolution())
 
-        
-"""   def test_backtracking_return_false_if_game2_cannot_be_solved(self):
+    def test_backtracking_return_false_if_game2_cannot_be_solved(self):
         Backtracking_Solver = BacktrackingSolver()
         Backtracking_Solver.load_puzzle(self.game2)
+        Backtracking_Solver.solve()
         self.assertFalse(Backtracking_Solver.solve())
-"""
+
 if __name__ == '__main__':
     unittest.main()
